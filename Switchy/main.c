94229386a29717de3ca932fa9e89019ec55cc54d
Art1xFX <kozakovi4@yandex.ru>
2022-06-14 19:46:05 +0300

    Refactoring

--- Switchy/main.c
+++ Switchy/main.c
@@ -1,31 +1,4 @@
-#include <Windows.h>
-#if _DEBUG
-#include <stdio.h>
-#endif // _DEBUG
-
-typedef NTSTATUS(WINAPI* RtlGetVersionPtr)(PRTL_OSVERSIONINFOW);
-
-typedef struct {
-	BOOL popup;
-} Settings;
-
-void ShowError(LPCSTR message);
-DWORD GetOSVersion();
-void PressKey(int keyCode);
-void ReleaseKey(int keyCode);
-void ToggleCapsLockState();
-LRESULT CALLBACK LowLevelKeyboardProc(int nCode, WPARAM wParam, LPARAM lParam);
-
-
-HHOOK hHook;
-BOOL enabled = TRUE;
-BOOL keystrokeProcessed = FALSE;
-BOOL winPressed = FALSE;
-
-Settings settings = {
-	.popup = FALSE
-};
-
+#include "main.h"
 
 int main(int argc, char** argv)
 {
@@ -67,13 +40,6 @@ int main(int argc, char** argv)
 	return 0;
 }
 
-
-void ShowError(LPCSTR message)
-{
-	MessageBox(NULL, message, "Error", MB_OK | MB_ICONERROR);
-}
-
-
 DWORD GetOSVersion()
 {
 	HMODULE hMod = GetModuleHandleW(L"ntdll.dll");
@@ -91,28 +57,6 @@ DWORD GetOSVersion()
 	}
 
 	return osvi.dwMajorVersion;
-}
-
-
-void PressKey(int keyCode)
-{
-	keybd_event(keyCode, 0, 0, 0);
-}
-
-
-void ReleaseKey(int keyCode)
-{
-	keybd_event(keyCode, 0, KEYEVENTF_KEYUP, 0);
-}
-
-
-void ToggleCapsLockState()
-{
-	PressKey(VK_CAPITAL);
-	ReleaseKey(VK_CAPITAL);
-#if _DEBUG
-	printf("Caps Lock state has been toggled\n");
-#endif // _DEBUG
 }
 
 
